b2080416f9d87f11b9d49cd94b2dac01e813d8f9
Abdillamit <abdillamit08@gmail.com>
2023-11-30 07:29:28 -0500

    start project

--- lib/src/features/authentication/screens/login.dart
+++ lib/src/features/authentication/screens/login.dart
@@ -8,11 +8,36 @@ class Login extends StatelessWidget {
       darkTheme: ThemeData(brightness: Brightness.dark),
       themeMode: ThemeMode.system,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Ваше приложение с логотипом'),
-        ),
-        body: Center(
-          child: Image.asset('assets/images/logo.svg'),
+        body: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 10.0, left: 10.0),
+                  width: 40.0,
+                  height: 40.0,
+                  child: Image.asset('assets/images/Logo.png'),
+                ),
+                SizedBox(height: 24.0),
+                Container(
+                  margin: EdgeInsets.only(left: 10.0),
+                  child: Text(
+                    'Финик Карта',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 10.0),
+                  child: Text(
+                    'Отмечай места на карте где нет нашего терминала, мы поставим его, а тебе пришлем бонусы которые ты сможешь обменять на реальные призы',
+                  ),
+                ),
+                SizedBox(height: 24.0),
+                TextButton(onPressed: () {}, child: Text('TextButton'))
+              ],
+            ),
+          ],
         ),
       ),
     );
